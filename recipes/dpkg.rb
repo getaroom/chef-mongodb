c8c8b9b32d6cd21494e999621be75452b5bf01a8
Ben Poweski <bpoweski@gmail.com>
2012-08-10 16:13:28 -0500

    remove basename from package_file

--- recipes/dpkg.rb
+++ recipes/dpkg.rb
@@ -1,6 +1,6 @@
-remote_file File.join(Chef::Config['file_cache_path'], File.basename(node['mongodb']['package_file'])) do
+remote_file File.join(Chef::Config['file_cache_path'], node['mongodb']['package_file']) do
   source node['mongodb']['url']
   action :create_if_missing
 end
 
-dpkg_package File.join(Chef::Config[:file_cache_path], File.basename(node['mongodb']['package_file']))
+dpkg_package File.join(Chef::Config[:file_cache_path], node['mongodb']['package_file'])
