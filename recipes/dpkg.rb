7ac17dbc67ea4bb50a31e2b28841bb0cbe96a5e4
Ben Poweski <bpoweski@gmail.com>
2012-08-10 16:44:00 -0500

    add instance setup logic from default to dpkg.rb

--- recipes/dpkg.rb
+++ recipes/dpkg.rb
@@ -4,3 +4,24 @@ remote_file File.join(Chef::Config['file_cache_path'], node['mongodb']['package_
 end
 
 dpkg_package File.join(Chef::Config[:file_cache_path], node['mongodb']['package_file'])
+
+needs_mongo_gem = (node.recipes.include?("mongodb::replicaset") or node.recipes.include?("mongodb::mongos"))
+
+if needs_mongo_gem
+  # install the mongo ruby gem at compile time to make it globally available
+  gem_package 'mongo' do
+    action :nothing
+  end.run_action(:install)
+  Gem.clear_paths
+end
+
+if node.recipes.include?("mongodb::default") or node.recipes.include?("mongodb")
+  # configure default instance
+  mongodb_instance "mongodb" do
+    mongodb_type "mongod"
+    port         node['mongodb']['port']
+    logpath      node['mongodb']['logpath']
+    dbpath       node['mongodb']['dbpath']
+    enable_rest  node['mongodb']['enable_rest']
+  end
+end
